1e037f0b3a7bcb6d3df933be598f198fb5ff3225
Benjamin Yakobi <benjaminyakobi@gmail.com>
2023-01-20 15:51:08 +0200

    Added test step to pipeline + first test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import Home from "../index";
+import Home from "../pages/index";
 
 describe("Home", () => {
   it("renders homepage unchanged", () => {
